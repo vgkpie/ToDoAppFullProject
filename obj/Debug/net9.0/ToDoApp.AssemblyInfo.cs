5dd1fb2df990bd375df2e3c56b5b55f46ec4013a
vgkpie <davidofv1056@gmail.com>
2025-05-02 13:22:59 +0300

    final

--- obj/Debug/net9.0/ToDoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/ToDoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7bd47b555ec3189613d05f65ab9228d85e3de40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b857a2d089ac946ab1037979584cd12a3620969b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
